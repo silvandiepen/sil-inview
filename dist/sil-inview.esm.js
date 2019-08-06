374b4933d02c3df8e4991c2cce0b3ff9bbc762fa
Sil van Diepen <me@silvandiepen.com>
2019-08-06 12:23:00 +0200

    Fix the positions and numbers

--- dist/sil-inview.esm.js
+++ dist/sil-inview.esm.js
@@ -1,4 +1,4 @@
-(function(){ if(typeof document !== 'undefined'){ var head=document.head||document.getElementsByTagName('head')[0], style=document.createElement('style'), css=""; style.type='text/css'; if (style.styleSheet){ style.styleSheet.cssText = css; } else { style.appendChild(document.createTextNode(css)); } head.appendChild(style); } })();
+/* eslint-disable */(function(){ if(typeof document !== 'undefined'){ var head=document.head||document.getElementsByTagName('head')[0], style=document.createElement('style'), css=""; style.type='text/css'; if (style.styleSheet){ style.styleSheet.cssText = css; } else { style.appendChild(document.createTextNode(css)); } head.appendChild(style); } })();
 var silInview = {
 	bind: function(el, binding) {
 		var observer;
@@ -6,31 +6,94 @@ var silInview = {
 			observerOptions: {
 				root: null,
 				rootMargin: '0px',
-				threshold: []
+				threshold: [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 			},
+			OGclasses: null,
+			setClasses: true,
+			setCustomProperties: true,
+			output: 'number',
+			classes: {
+				inview: 'is-inview',
+				notinview: 'is-not-inview',
+				below: 'is-below-view',
+				above: 'is-above-view'
+			},
+			customProperties: {
+				inview: 'inview'
+			},
+			centered: true,
 			active: false,
 			debug: false
 		};
 		var actions = {
 			init: function () {
-				if(!binding.value) { return; }
+				if (el.classList.length > 0) {
+					settings.ogclasses = el.classList;
+				}
+				if (!binding.value) { return; }
 				else {
-					if(binding.value.root) { settings.observerOptions.root = binding.value.root; }
-					if(binding.value.margin) { settings.observerOptions.rootMargin = binding.value.margin; }
-					if(binding.value.treshold) { settings.observerOptions.treshold = binding.value.treshold; }
+					if (binding.value.root)
+						{ settings.observerOptions.root = binding.value.root; }
+					if (binding.value.margin)
+						{ settings.observerOptions.rootMargin = binding.value.margin; }
+					if (binding.value.treshold)
+						{ settings.observerOptions.treshold = binding.value.treshold; }
+					if (binding.value.active) { settings.active = binding.value.active; }
+					if (binding.value.setClasses)
+						{ settings.setClasses = binding.value.setClasses; }
+					if (binding.value.setCustomProperties)
+						{ settings.setCustomProperties = binding.value.setCustomProperties; }
+					if (binding.value.output) { settings.output = binding.value.output; }
+				}	
+				if(!binding.value.treshhold){
+					var thresholdSets = [];
+					for (var i=0; i<=1.0; i+= 0.01) {
+						thresholdSets[0].push(i);
+					}
+					settings.observerOptions.treshhold = thresholdSets;
+				} else {
+					settings.observerOptions.treshhold = binding.value.treshhold;
 				}
 			},
 			observer: function () {
-				observer = new IntersectionObserver(actions.set,settings.observerOptions);
-				observer.observe(el); 
+				observer = new IntersectionObserver(actions.set, settings.observerOptions);
+				observer.observe(el);
 			},
 			set: function (entry) {
-				console.log(entry, el);
+				var outputValue = 0;
+				var inview = entry[0].intersectionRatio;
+				
+				if(settings.centered){
+					if(settings.output == 'percentage') { outputValue = (Math.floor(inview * 100) - 100) + '%'; }
+					if(settings.output == 'number') { outputValue = Math.round(inview * 100) / 100; }					
+				} else {
+					if(settings.output == 'percentage') { outputValue = Math.floor(inview * 100) + '%'; }
+					if(settings.output == 'number') { outputValue = Math.round(inview * 100) / 100; }				
+				}
+
+				if (settings.setClasses) {
+					if (inview > 0) { el.classList = actions.classes('notinview'); }
+					else { el.classList = actions.classes('notinview'); }
+				}
+
+				if (settings.setCustomProperties) {
+					el.style.setProperty(("--" + (settings.customProperties.inview)),outputValue);
+				}
+			},
+			classes: function (status) {
+				var classes = [];
+				switch (status) {
+					case 'inview':
+						classes.push(settings.classes.inview);
+					case 'notinview':
+						classes.push(settings.classes.notinview);
+				}
+				if (settings.ogclasses) { classes.push(settings.ogclasses); }
+				return classes.join(' ');
 			}
 		};
 		actions.init();
 		actions.observer();
-
 	}
 };
 
--- src/sil-inview.vue
+++ src/sil-inview.vue
@@ -6,31 +6,95 @@ export default {
 			observerOptions: {
 				root: null,
 				rootMargin: '0px',
-				threshold: []
+				threshold: [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 			},
+			OGclasses: null,
+			setClasses: true,
+			setCustomProperties: true,
+			output: 'percentage',
+			classes: {
+				inview: 'is-inview',
+				notinview: 'is-not-inview',
+				below: 'is-below-view',
+				above: 'is-above-view'
+			},
+			customProperties: {
+				inview: 'inview'
+			},
+			centered: true,
 			active: false,
 			debug: false
 		};
 		let actions = {
 			init: () => {
-				if(!binding.value) return;
+				if (el.classList.length > 0) {
+					settings.ogclasses = el.classList;
+				}
+				if (!binding.value) return;
 				else {
-					if(binding.value.root) settings.observerOptions.root = binding.value.root;
-					if(binding.value.margin) settings.observerOptions.rootMargin = binding.value.margin;
-					if(binding.value.treshold) settings.observerOptions.treshold = binding.value.treshold;
+					if (binding.value.root)
+						settings.observerOptions.root = binding.value.root;
+					if (binding.value.margin)
+						settings.observerOptions.rootMargin = binding.value.margin;
+					if (binding.value.treshold)
+						settings.observerOptions.treshold = binding.value.treshold;
+					if (binding.value.active) settings.active = binding.value.active;
+					if (binding.value.setClasses)
+						settings.setClasses = binding.value.setClasses;
+					if (binding.value.setCustomProperties)
+						settings.setCustomProperties = binding.value.setCustomProperties;
+					if (binding.value.output) settings.output = binding.value.output;
+				}	
+				if(!binding.value.treshhold){
+					let thresholdSets = [];
+					for (let i=0; i<=1.0; i+= 0.01) {
+						thresholdSets[0].push(i);
+					}
+					settings.observerOptions.treshhold = thresholdSets;
+				} else {
+					settings.observerOptions.treshhold = binding.value.treshhold;
 				}
 			},
 			observer: () => {
-				observer = new IntersectionObserver(actions.set,settings.observerOptions);
-				observer.observe(el); 
+				observer = new IntersectionObserver(actions.set, settings.observerOptions);
+				observer.observe(el);
 			},
 			set: (entry) => {
-				console.log(entry, el);
+				let outputValue = 0;
+				let inview = entry[0].intersectionRatio;
+				let percentage = Math.floor(inview * 100);
+				
+				if(settings.centered){
+					if(settings.output == 'percentage') { outputValue = (Math.floor(inview * 100) - 100) + '%'; }
+					if(settings.output == 'number') { outputValue = Math.round(inview * 100) / 100; }					
+				} else {
+					if(settings.output == 'percentage') { outputValue = Math.floor(inview * 100) + '%'; }
+					if(settings.output == 'number') { outputValue = Math.round(inview * 100) / 100; }				
+				}
+
+				if (settings.setClasses) {
+					if (inview > 0) el.classList = actions.classes('notinview');
+					else el.classList = actions.classes('notinview');
+				}
+
+				if (settings.setCustomProperties) {
+					el.style.setProperty(`--${settings.customProperties.inview}`,outputValue);
+				}
+			},
+			classes: (status) => {
+				let classes = [];
+				switch (status) {
+					case 'inview':
+						classes.push(settings.classes.inview);
+					case 'notinview':
+						classes.push(settings.classes.notinview);
+				}
+				if (settings.ogclasses) classes.push(settings.ogclasses);
+				return classes.join(' ');
 			}
-		}
+		};
 		actions.init();
 		actions.observer();
-
 	}
 };
 </script>
